7e78da8ed7fc5098da6e120c7184c03179e14deb
Maks Light <grizckam@ya.ru>
2016-03-29 19:14:39 +0300

    Add specialists

--- script/by_scripts.js
+++ script/by_scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
 	$('.carousel-indicators').hide();
+	$('.hide_block_all_specialists').hide();
+
+
+
+	$('.open_all').click(function(){	
+
+				$('.hide_block_all_specialists').slideDown(1000);
+				$('.open_all').text('Закрыть всех');
+				});
+
+
 	});
