4ade32c0378a935d5806b58feda58be4d0ab2926
Maks Light <grizckam@ya.ru>
2016-03-30 20:30:27 +0300

    Add footer, add button 'open-close' of block by specialists

--- script/by_scripts.js
+++ script/by_scripts.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
 	$('.carousel-indicators').hide();
 	$('.hide_block_all_specialists').hide();
-
-
-
+	$('.close_all').hide();
+	$('.jcarousel-control-prev').hide();
+	$('.jcarousel-control-next').hide();
+	
 	$('.open_all').click(function(){	
 
 				$('.hide_block_all_specialists').slideDown(1000);
-				$('.open_all').text('Закрыть всех');
+				$('.open_all').hide();
+				$('.close_all').show();
 				});
+	$('.close_all').click(function(){	
 
+				$('.hide_block_all_specialists').slideUp(1000);
+				$('.close_all').hide();
+				$('.open_all').show();
+				});
 
 	});
