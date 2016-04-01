85738b2c86a4fd05be896d9835aeb2c368f4ae69
Maks Light <grizckam@ya.ru>
2016-04-01 17:17:56 +0300

    Added effects to all blocks, edit some things

--- script/by_scripts.js
+++ script/by_scripts.js
@@ -2,9 +2,45 @@ $(document).ready(function () {
 	$('.carousel-indicators').hide();
 	$('.hide_block_all_specialists').hide();
 	$('.close_all').hide();
-	$('.jcarousel-control-prev').hide();
-	$('.jcarousel-control-next').hide();
+	$('.list-services').hide();
+
+	$('.title_about_us').hide();
+	$('.all_about_us_big_block').hide();
+	$('.title_all_services').hide();
+	$('.all_mini_blocks').hide();
+	$('.our_specialists_block').hide();
+	$('.all_big_recalls').hide();
+	$('.time_work').hide();
+
+	$('.list-services').fadeIn(2000);
+
+	/* slide all blocks*/
+	$(window).scroll(function () {
+	if ($(this).scrollTop() > 100) {
+	$('.title_about_us').slideDown(1000);
+    }
+    if ($(this).scrollTop() > 200) {
+     $('.all_about_us_big_block').fadeIn(2000);
+    }
+
+
+	if ($(this).scrollTop() > 400) {
+	$('.title_all_services').slideDown(1000);
+    }
+
+    if ($(this).scrollTop() > 600) {
+     $('.all_mini_blocks').fadeIn(2000);
+    }
+     if ($(this).scrollTop() > 700) {
+     $('.our_specialists_block').fadeIn(2000);
+    }
+      if ($(this).scrollTop() > 1000) {
+     $('.all_big_recalls').fadeIn(1000);
+    }
+    });
+	/* END slide all blocks*/
 	
+	/* Open all specialists*/
 	$('.open_all').click(function(){	
 
 				$('.hide_block_all_specialists').slideDown(1000);
@@ -17,5 +53,22 @@ $(document).ready(function () {
 				$('.close_all').hide();
 				$('.open_all').show();
 				});
+/* END Open all specialists*/
+
+/* Hover block time_work of specialists*/
+$('.specialist').hover(
+      function () {
+        $(this).children('.time_work').slideDown(500);
+      },
+      function () {
+        $(this).children('.time_work').slideUp(500);
+      }
+    );
+/* END Hover block time_work of specialists*/
+
+
 
 	});
+
+
+	
